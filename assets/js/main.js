41d28de92fb5d5dafd7d0717814cab32cb483041
arturgevorgyan <frontdevart@gmai.com>
2020-05-25 13:33:32 +0400

    Intro Compleated

---
+++ assets/js/main.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const burger = document.querySelector('.yoga_container > .not_changed > .header_dots > .header > .burger')
+    const close = document.querySelector('.yoga_container > .not_changed > .sidebar > .haader > .close')
+    const sidebar = document.querySelector('.yoga_container > .not_changed > .sidebar')
+    const overlay = document.querySelector('.overlay')
+
+    const sliderMain = new Slider({
+        dotsCountComeFrom: '.sidebar > ul.list > .slide_dot',
+        rootOneItemDotClass: 'dot dis_al slide_dot',
+        rootOneItemDotChildClassNode: 'circle',
+        slideItems: '.mainSldes.slider_container',
+        parentOfGeneretedList: '.dots_container_inner',
+        hasOtherElemet: true,
+        canUseSliderDots: ['.sidebar > ul.list > .slide_dot']
+    })
+
+    const sliderIntro = new Slider({
+        dotsCountComeFrom: '.intro .intro_slides > .intro_slide',
+        rootOneItemDotClass: 'dot slide_dot',
+        rootOneItemDotChildClassNode: 'circle',
+        slideItems: '.intro .slider_container',
+        parentOfGeneretedList: '.intro .dots'
+    })
+
+    sliderMain.generateDots()
+    sliderIntro.generateDots()
+
+    const sideBarList = document.querySelectorAll('.sidebar > ul.list > .slide_dot')
+    const homeLeftDotList = document.querySelectorAll('.dots_container_inner .number_name')
+    
+    function changeDotText(){
+        homeLeftDotList.forEach((node, index) => {
+            node.addEventListener('mouseover', function() {
+                this.textContent = sideBarList[index].textContent
+            })
+            node.addEventListener('mouseout', function() {
+                this.textContent = homeLeftDotList.length < 10 ? `0${String(index + 1)}` : String(index + 1)
+            })
+        })
+    }
+    
+    changeDotText()
+
+    function closeSidebar(){
+        sidebar.classList.remove('active')
+        overlay.classList.remove('active')
+    }
+
+    burger.addEventListener('click', () => {
+        sidebar.classList.add('active')
+        overlay.classList.add('active')
+
+    })
+    close.addEventListener('click', closeSidebar)
+    document.addEventListener('click', e => {
+        if(e.target.classList.contains('overlay')){
+            closeSidebar()
+        }
+    })
+})
---
+++ assets/js/slider.js
@@ -0,0 +1,67 @@
+class Slider {
+    constructor(options) {
+        this.dotsCountComeFrom = document.querySelectorAll(options.dotsCountComeFrom)
+        this.rootOneItemDotClass = options.rootOneItemDotClass
+        this.rootOneItemDotChildClassNode = options.rootOneItemDotChildClassNode
+        this.parentOfGeneretedList = document.querySelector(options.parentOfGeneretedList)
+        this.slideItems = Array.from(document.querySelector(options.slideItems).children)
+        if(options.hasOtherElemet){
+            this.hasOtherElemet = options.hasOtherElemet
+        }
+        if(options.canUseSliderDots && options.canUseSliderDots.length){
+            this.canUseSliderDots = options.canUseSliderDots
+        }
+
+    }
+    generateDots() {
+        this.dotsCountComeFrom.forEach((node, index) => {
+            const dot = document.createElement('div')
+            dot.className = this.rootOneItemDotClass
+    
+            const circle = document.createElement('div')
+            circle.className = this.rootOneItemDotChildClassNode
+    
+            dot.appendChild(circle)
+    
+            if(this.hasOtherElemet){
+                const number_name = document.createElement('div')
+                number_name.className = 'number_name'
+                number_name.textContent = index + 1 < 10 ? `0${String(index + 1)}` : String(index + 1)
+    
+                dot.appendChild(number_name)
+            }
+    
+            this.parentOfGeneretedList.appendChild(dot)
+        })
+        this.slide(this.parentOfGeneretedList.children)
+    }
+    slide(listofGeneretedDots){
+        const slideArray = []
+        if(this.canUseSliderDots){
+            this.canUseSliderDots.forEach(selector => {
+                const newDotUser = Array.from(document.querySelectorAll(selector))
+                slideArray.push(newDotUser)
+            })
+        }
+        slideArray.push(Array.from(listofGeneretedDots))
+
+        slideArray.forEach((nodes) => {
+            nodes[0].classList.add('active')
+            nodes.forEach((node, index) => {
+                node.addEventListener('click', () => {
+                    nodes.forEach(node => node.classList.remove('active'))
+                    node.classList.add('active')
+                    this.addClass(index)
+                })
+            })
+        })
+    }
+    addClass(index) {
+        if(this.slideItems[index]) {
+            this.slideItems.forEach(slide => {
+                slide.classList.remove('active')
+            })
+            this.slideItems[index].classList.add('active')
+        }
+    }
+}
